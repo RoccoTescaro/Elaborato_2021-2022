f4a6e501f6d26cd03a606365e9a27afee64b0150
roccotescaro <rocco.tescaro@gmail.com>
2022-03-14 10:06:37 +0100

    State class creation

---
+++ headers/State.h
@@ -0,0 +1,14 @@
+//
+// Created by roccotescaro on 14/03/22.
+//
+
+#ifndef ELABORATOPROGRAMMAZIONE2021_STATE_H
+#define ELABORATOPROGRAMMAZIONE2021_STATE_H
+
+
+class State {
+
+};
+
+
+#endif //ELABORATOPROGRAMMAZIONE2021_STATE_H
---
+++ src/State.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roccotescaro on 14/03/22.
+//
+
+#include "State.h"
