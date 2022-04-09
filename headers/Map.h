c478cfbbbe12ced8071a395a14ff7232f08aeb55
Rocco Tescaro <rocco.tescaro@stud.unifi.it>
2022-04-09 04:23:59 +0200

    Completed Map implementation, and implemented a bounch of log functions to make easier to debug. Still some fixes to implement (posToKeyConverter function makes implicit cast that shouldnt makes)

--- headers/Map.h
+++ headers/Map.h
@@ -1,13 +1,40 @@
 #ifndef MAP_H
 #define MAP_H
-
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
 #include <fstream>
 
-class Entity {};
-class Tile : public Entity {};
+class Entity {
+public:
+	Entity(sf::Vector2<int> pos_) : pos(pos_) {};
+	
+	void render(sf::RenderWindow& window) {
+		window.draw(sprite);
+	};
+protected:
+	sf::Vector2<int> pos;
+	sf::RectangleShape sprite;
+};
+class Tile : public Entity {
+public:
+	Tile(sf::Vector2<int> pos_, const sf::Vector2<float>& cellDim) : Entity(pos_) {
+		sprite.setPosition(pos_.x * cellDim.x, pos_.y * cellDim.y);
+		sprite.setSize(cellDim);
+	}	
+};
 class GameCharacter : public Entity {};
+class Wall : public Tile {
+public:
+	Wall(sf::Vector2<int> pos_, const sf::Vector2<float>& cellDim) : Tile(pos_, cellDim) {
+		sprite.setFillColor(sf::Color(255,0,0,255));
+	}
+};
+class Hole : public Tile {
+public:
+	Hole(sf::Vector2<int> pos_, const sf::Vector2<float>& cellDim) : Tile(pos_, cellDim) {
+		sprite.setFillColor(sf::Color(0, 255, 0, 255));
+	}
+};
 
 class Map {
 public:
@@ -15,23 +42,20 @@ public:
 	enum entityType { 
 		wall = 'W',
 		hole = 'H',
-		workingTable = 'T',
-		drawable = 'D',
-		npc = 'N',
-		enemy = 'E',
-		player = 'P'
+		empty = ' '
 	};
 
 	void load(std::fstream& file);
 
 	void appendEntity(char type, int x, int y);
 
-	//#TODO void render();
+	void appendEntity(GameCharacter* character, int x, int y);
 
-	//#TODO void move();
+	void render(sf::RenderWindow& window);
+
+	void move(const sf::Vector2<int>& start, const sf::Vector2<int>& end);
 
 	Entity* operator()(uint32_t pos, Map::entityLayer layer);
-	Entity* operator()(int x, int y, Map::entityLayer layer);
 	Entity* operator()(float x, float y, Map::entityLayer layer);
 
 private:
@@ -43,7 +67,8 @@ private:
 	std::unordered_map<uint32_t, std::shared_ptr<GameCharacter>> gameCharacters;
 
 	sf::Vector2<int> dim;
-	const sf::Vector2<float> cellDim{48.f,48.f}; //#TODO float or int
+	
+	const sf::Vector2<float> cellDim{ 48.f, 48.f }; //#TODO float or int
 };
 
 
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,14 +1,21 @@
 #include "../headers/Map.h"
 #include <iostream>
+#include "../headers/Log.h"
 
 void Map::load(std::fstream& file) {
 	if (file.is_open()) {
 		std::string firstLine;
 		std::getline(file, firstLine);
-		dim = { stoi(firstLine.substr(0,firstLine.find(' '))), stoi(firstLine.substr(firstLine.find(' '),firstLine.size()))};
+		auto space = firstLine.find(' ');
+		if (firstLine.size() <= 0 || space == std::string::npos) {
+			ERROR("loading - map file has a wrong format");
+			return;
+		}
+		dim = { stoi(firstLine.substr(0,space)), stoi(firstLine.substr(space+1,firstLine.size()))};
 		for (int j = 0; j < dim.y; ++j) {
 			std::string line;
-			if(std::getline(file, line).eofbit) break;
+			std::getline(file, line);
+			if(line.size() <= 0) break;
 			for (int i = 0; i < dim.x; ++i) {
 				if (i >= line.size()) break;
 				appendEntity(line[i],i,j);
@@ -17,26 +24,62 @@ void Map::load(std::fstream& file) {
 		file.close();
 	}
 	else
-		std::cout << "ERROR: loading-map file has not been open" << std::endl;
+		ERROR("loading - map file has not been open");
 }
 
 void Map::appendEntity(char type, int x, int y){
-	switch (type) {
-	case entityType::wall: {
-		tiles.emplace(posToKeyConverter(x,y),new Tile); //WALL 
-		break;
+	if (x < dim.x && x >= 0 && y < dim.y && y >= 0) {
+		switch (type) {
+		case entityType::wall: {
+			DEBUG("Wall allocated, key:{%}", posToKeyConverter(x, y));
+			tiles.emplace(posToKeyConverter(x, y), new Wall({ x,y }, cellDim));
+			break;
+		}
+		case entityType::hole: {
+			DEBUG("Hole allocated, key:{%}",posToKeyConverter(x, y));
+			tiles.emplace(posToKeyConverter(x, y), new Hole({ x,y }, cellDim));
+			break;
+		}
+		case entityType::empty: {
+			break;
+		}
+		default: {
+			ERROR("trying to istanciate an unknown type");
+			break;
+		}
+		}
 	}
-	case entityType::player: {
-		break;
-	}
-	default: {
-		std::cout << "ERROR: trying to istanciate an unknown type" << std::endl;
-		break;
-	}
-	}
+	else
+		ERROR("trying to allocate an Entity but non map has been loaded yet");
 }
 
-Entity* Map::operator()(uint32_t pos, Map::entityLayer layer = gameCharacterLayer) {
+void Map::appendEntity(GameCharacter* character, int x, int y) {
+	gameCharacters.emplace(posToKeyConverter(x, y), character);
+}
+
+void Map::render(sf::RenderWindow& window) {
+	auto& view = window.getView(); 
+	for (float j = std::max<float>(view.getCenter().y - view.getSize().y*0.5f -cellDim.y, 0.f); j < std::min<float>(view.getCenter().y + view.getSize().y * 0.5 + cellDim.y,dim.y*cellDim.y); j += cellDim.y)
+		for (float i = std::max<float>(view.getCenter().x - view.getSize().x * 0.5f - cellDim.x, 0.f); i < std::min<float>(view.getCenter().x + view.getSize().x * 0.5f + cellDim.x,dim.x*cellDim.y); i += cellDim.x) {
+			auto tile = Map::operator()(i, j, Map::tileLayer);
+			if (tile) tile->render(window);
+			auto gameCharater = Map::operator()(i,j,Map::gameCharacterLayer);
+			if (gameCharater) gameCharater->render(window);
+		}
+}
+
+void Map::move(const sf::Vector2<int>& start, const sf::Vector2<int>& end) {
+	if (Map::operator()(posToKeyConverter(start.x, start.y), Map::gameCharacterLayer)) { //#TODO fix conversion
+		auto character = static_cast<GameCharacter*>(Map::operator()(posToKeyConverter(start.x, start.y), Map::gameCharacterLayer));
+		gameCharacters.erase(posToKeyConverter(start.x, start.y));
+		appendEntity(character, end.x, end.y);
+	}
+	else
+		ERROR("no Entity in starting tile");
+}
+
+
+ Entity* Map::operator()(uint32_t pos, Map::entityLayer layer = gameCharacterLayer) {
 	if (layer == gameCharacterLayer) {
 		auto entity = gameCharacters.find(pos);
 		if (entity != gameCharacters.end()) return entity->second.get();
@@ -46,10 +89,6 @@ Entity* Map::operator()(uint32_t pos, Map::entityLayer layer = gameCharacterLaye
 		if (entity != tiles.end()) return entity->second.get();
 	}
 	return NULL;
-}
-
-Entity* Map::operator()(int x, int y, Map::entityLayer layer = gameCharacterLayer) {
-	return Map::operator()(posToKeyConverter(x,y), layer);
 }
 
 Entity* Map::operator()(float x, float y, Map::entityLayer layer = gameCharacterLayer) {
@@ -67,6 +106,5 @@ uint32_t Map::posToKeyConverter(int a, int b) {
 
 uint32_t Map::posToKeyConverter(float a, float b) {
 	if (a < 0 || b < 0) return 0;
-	return posToKeyConverter(uint16_t(uint16_t(a / cellDim.x) * cellDim.x),
-							 uint16_t(uint16_t(a / cellDim.y) * cellDim.y));
+	return posToKeyConverter(uint16_t(a / cellDim.x), uint16_t(b / cellDim.y));
 }
