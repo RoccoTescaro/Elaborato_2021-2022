7ce5aba06249b7b18c919fb2a803817d4f40e697
Carnis_ <lapo.bernacchioni@stud.unifi.it>
2022-05-05 15:10:31 +0200

    pathfinding: aggiunte classi 'pathAlgorithm e derivate'

--- headers/Pathfinding.h
+++ headers/Pathfinding.h
@@ -2,18 +2,61 @@
 
 #ifndef PATHFINDING_H
 #define PATHFINDING_H
+#include <SFML/Graphics.hpp>
+#include <list>
+#include "Player.h"
+
+using namespace sf;
 
 class Map{};
 
-class PathAlgorithm{};
+class PathAlgorithm{
+    public:
+        PathAlgorithm(Map &map): map(map){}
+        virtual std::list<Vector2i> findPath(Vector2i start, Vector2i end, bool flying)=0;
+    private:
+        Map &map;
+};
+
+class A_Star:public PathAlgorithm{
+    public:
+        A_Star(Map &map): PathAlgorithm(map){}
+        std::list<Vector2i> findPath(Vector2i start, Vector2i end, bool flying) override;
+};
+
+class DiglettMovement:public PathAlgorithm{
+    public:
+        DiglettMovement(Map &map): PathAlgorithm(map){}
+        std::list<Vector2i> findPath(Vector2i start, Vector2i playerPos, bool flying) override;
+};
 
 class  Pathfinding{
     public:
-        Pathfinding(Map &map):map(map), aStar(PathAlgorithm()){
-        }
+        Pathfinding(Map &map,Player &Player):aStar(A_Star(map)),  map(map),dMovement(DiglettMovement(map)){}
+        std::list<Vector2i> findPath(Vector2i subject, Vector2i target); 
+                                /*  if PLAYER
+                                        aStar:
+                                        target = click position;
+                                        flying = false;
+
+
+                                    if ENEMY
+
+                                        if DIGLETT                      i diglet non usano il target come  
+                                            dMovement                   casella di destinazione macome riferimento                    
+                                            target = playerpos          per il calcolo della cella bersaglio
+                                            flaying = false
+                                        else
+                                            aStar
+                                            target = playerpos
+                                            flaying = false
+                                */
+
     private:
+        A_Star aStar;
+        DiglettMovement dMovement;
         Map &map;
-        PathAlgorithm aStar; 
+
 
 
 };
--- src/Pathfinding.cpp
+++ src/Pathfinding.cpp
@@ -1,1 +1,6 @@
 #include "../headers/Pathfinding.h"
+
+
+std::list<Vector2i> Pathfinding::findPath(Vector2i subject, Vector2i target){
+
+}
