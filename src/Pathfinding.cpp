4f71355f53da95258cce0911f440a8f02d932888
LapoBernacchioni <LapoBernacchioni@github.com>
2022-05-09 13:14:23 +0200

    Entity duplication in map resolved: GC Branch update

--- src/Pathfinding.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/Pathfinding.h"
-
-
-std::list<Vector2i> Pathfinding::findPath(Vector2i subject, Vector2i target){
-    
-    return aStar.findPath(subject,target,map[subject].canFly());
-
-    
-}
